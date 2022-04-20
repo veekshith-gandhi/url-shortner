e448bbb03d92433e302b471b3ed577c7d16a5b90
veekshith-gandhi <veekshithgandhi@gmail.com>
2022-04-21 01:11:12 +0530

    bug

--- models/shorturl.model.js
+++ models/shorturl.model.js
@@ -5,7 +5,7 @@ const shortId = require("shortid");
 
 const ShortUrlSchema = new mongoose.Schema({
   full: { type: String, required: true },
-  short: { type: String, required: true, default: shortId.generate() },
+  short: { type: String, required: true, default: shortId.generate },
   clicks: { type: Number, required: true, default: 0 },
 });
 
